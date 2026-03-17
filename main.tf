d43b798b0e1534223b6520f2e590ecc5a449505b
talbal24 <134201605+talbal24@users.noreply.github.com>
2026-03-17 16:32:04 +0200

    Add second VCS environment and update outputs
    Added a second VCS-based environment with updated settings and output.

--- main.tf
+++ main.tf
@@ -13,43 +13,52 @@ provider "env0" {
   # ENV0_API_SECRET
 }
 
-# Variable for your project ID
 variable "project_id" {
   description = "env0 Project ID"
   type        = string
 }
 
-# Environment: Created from VCS (without template)
+# Environment 1: Created from VCS
 resource "env0_environment" "vcs_environment" {
   name       = "my-new-env"
   project_id = var.project_id
   workspace  = "my-vcs-workspace"
-  
+
   without_template_settings {
-    repository           = "https://github.com/talbal24/tal-gh-demo"
-    type                 = "terraform"
-    terraform_version    = "1.5.0"
-    vcs_connection_id = "c0956ba9-012c-490e-a2bd-d97d97489a74"
+    repository         = "https://github.com/talbal24/tal-gh-demo"
+    type               = "terraform"
+    terraform_version  = "1.5.0"
+    vcs_connection_id  = "c0956ba9-012c-490e-a2bd-d97d97489a74"
   }
-  resource "env0_environment" "vcs_environment" {
-  name       = "my-other-env"
-  project_id = var.project_id
-  workspace  = "my-vcs-workspace"
-  
-  without_template_settings {
-    repository           = "https://github.com/talbal24/tal-gh-demo"
-    type                 = "terraform"
-    terraform_version    = "1.5.0"
-    vcs_connection_id = "c0956ba9-012c-490e-a2bd-d97d97489a74"
-  }
-  # Optional VCS settings
+
   deploy_on_push                   = true
   run_plan_on_pull_requests        = true
   auto_deploy_on_path_changes_only = true
 }
 
-# Output to see the created environment ID
+resource "env0_environment" "other_vcs_environment" {
+  name       = "my-other-env"
+  project_id = var.project_id
+  workspace  = "my-other-vcs-workspace"
+
+  without_template_settings {
+    repository         = "https://github.com/talbal24/tal-gh-demo"
+    type               = "terraform"
+    terraform_version  = "1.5.0"
+    vcs_connection_id  = "c0956ba9-012c-490e-a2bd-d97d97489a74"
+  }
+
+  deploy_on_push                   = true
+  run_plan_on_pull_requests        = true
+  auto_deploy_on_path_changes_only = true
+}
+
 output "vcs_environment_id" {
   value       = env0_environment.vcs_environment.id
-  description = "ID of the VCS-based environment"
+  description = "ID of the first VCS-based environment"
+}
+
+output "other_vcs_environment_id" {
+  value       = env0_environment.other_vcs_environment.id
+  description = "ID of the second VCS-based environment"
 }
