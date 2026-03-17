9b06d6b9e491e334af73b370790ac2d49b81fbd8
talbal24 <134201605+talbal24@users.noreply.github.com>
2026-03-17 15:13:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "env0_environment" "vcs_environment" {
     repository           = "https://github.com/talbal24/tal-gh-demo"
     type                 = "terraform"
     terraform_version    = "1.5.0"
-    vcsConnectionId = "c0956ba9-012c-490e-a2bd-d97d97489a74"
+    token_id  = "c0956ba9-012c-490e-a2bd-d97d97489a74"
   }
   
   # Optional VCS settings
